277e8ae51fc64d33f0a83fdaaa6f40e0fc305baa
Hamid Asadi <hamid.19471@gmail.com>
2024-07-14 19:25:57 +0330

    Update: config brand colors in main tailwindcss file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,27 @@
 import type { Config } from "tailwindcss";
 
+const colors: Record<string, string> = {
+  "main-bg": "#F5F2EA",
+  "footer-bg": "#F7EFE2",
+  content: "#2D3556",
+  "heading-color": "#222222",
+  white: "#ffff",
+  black: "#000",
+  success: "#36d399",
+  warning: "#fbbd23",
+  error: "#f87272",
+  info: "#3abff8",
+  neutral: "#2a323c",
+  accent: "#1FB2A5",
+  secondary: "#6c5ce7",
+  "base-25": "#353d47",
+  "base-50": "#2a323c",
+  "base-75": "#20272e",
+  "base-100": "#1d232a",
+  "base-200": "#191e24",
+  "base-300": "#15191e",
+};
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,13 +29,8 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    colors: colors,
+    extend: {},
   },
   plugins: [],
 };
