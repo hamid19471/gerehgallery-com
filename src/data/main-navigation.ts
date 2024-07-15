61b6ccf6ed9579b49df831e53966e81cc4a7622c
Hamid Asadi <hamid.19471@gmail.com>
2024-07-15 15:40:38 +0330

    Create: header navigation data file

---
+++ src/data/main-navigation.ts
@@ -0,0 +1,36 @@
+import { Navigations } from "../types/navigation.types";
+
+export const HeaderNav: Navigations[] = [
+  {
+    title: "Decorative",
+    href: "/categories/decorative",
+  },
+  {
+    title: "Traditional",
+    href: "/categories/traditional",
+  },
+  {
+    title: "Persian Rugs",
+    href: "/categories/persian-rugs",
+  },
+  {
+    title: "Gereh Styles",
+    href: "/categories/gereh-styles",
+  },
+  {
+    title: "Shop",
+    href: "/shop",
+  },
+  {
+    title: "About us",
+    href: "/about-us",
+  },
+  {
+    title: "Contact",
+    href: "/contact-us",
+  },
+  {
+    title: "Blog",
+    href: "/blog",
+  },
+];
