76c3d465c98c4e3aa18d2eb35792d1943a69ccb9
Hamid Asadi <hamid.19471@gmail.com>
2024-07-16 19:42:32 +0330

    Update: import slider component in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import { Slider } from "./_components/slider";
 
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <Slider />
+    </div>
+  );
 }
