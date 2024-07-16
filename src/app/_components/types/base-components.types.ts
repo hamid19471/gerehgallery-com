42257300409b2d16f39a0ecddc087aaca0a120dc
Hamid Asadi <hamid.19471@gmail.com>
2024-07-16 19:41:04 +0330

    Create: globla components types

---
+++ src/app/_components/types/base-components.types.ts
@@ -0,0 +1,9 @@
+import { Size } from "./size.types";
+import { Variant } from "./variant.types";
+
+export type BaseComponents = {
+  variant?: Variant;
+  size?: Size;
+  className?: string;
+  isDisable?: boolean;
+};
---
+++ src/app/_components/types/variant.types.ts
@@ -0,0 +1,6 @@
+export type Variant =
+  | "primary"
+  | "secondary"
+  | "success"
+  | "warning"
+  | "error"
