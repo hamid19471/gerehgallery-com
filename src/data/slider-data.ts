3f1d8f8131f8a5eff17e5b48a6f95d56c636e7c4
Hamid Asadi <hamid.19471@gmail.com>
2024-07-16 19:41:20 +0330

    Create: dummy slider data

---
+++ src/data/slider-data.ts
@@ -0,0 +1,25 @@
+import { SliderProps } from "@/app/_components/slider/slider.types";
+
+export const SliderData: SliderProps[] = [
+  {
+    id: 1,
+    title: "Summer Sale collection",
+    description: "Sale! Up to 50% off",
+    image: "/images/product.webp",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "Summer Sale collection",
+    description: "Sale! Up to 50% off",
+    image: "/images/product.webp",
+    url: "/",
+  },
+  {
+    id: 3,
+    title: "Summer Sale collection",
+    description: "Sale! Up to 50% off",
+    image: "/images/product.webp",
+    url: "/",
+  },
+];
