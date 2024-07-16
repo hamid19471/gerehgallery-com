24f10213ef602e93f462a81b8efd968f31a30735
Hamid Asadi <hamid.19471@gmail.com>
2024-07-16 19:41:52 +0330

    Create: cart modal feature

---
+++ src/app/_components/cart-modal/cart-modal.tsx
@@ -0,0 +1,52 @@
+"use client";
+import React, { ReactNode, useEffect, useRef } from "react";
+import { Button } from "../button";
+
+type Props = {
+  children: ReactNode;
+  setShopCart: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export const CartModal: React.FC<Props> = ({ children, setShopCart }) => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    const clickOutSide = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        setShopCart(false);
+      }
+    };
+    document.addEventListener("mousedown", clickOutSide);
+
+    return () => {
+      document.removeEventListener("mousedown", clickOutSide);
+    };
+  }, [setShopCart]);
+  return (
+    <div
+      ref={ref}
+      className="w-[330px]  z-50 rounded-lg absolute top-8 left-0 shadow-lg bg-white p-4"
+    >
+      <h3 className="font-semibold text-lg border-b border-base-25/10 pb-2">
+        Shopping Cart
+      </h3>
+
+      {children}
+      <div className="flex flex-col w-full gap-2 border-b border-t border-base-25/10 py-4 my-6">
+        <div className="flex items-center justify-between">
+          <h4 className="font-bold text-lg">Subtotal</h4>
+          <p className="font-bold">300$</p>
+        </div>
+        <p className="text-xs text-base-50 font-light">
+          Shipping and taxs calculated at checkout
+        </p>
+      </div>
+
+      <div className="flex items-center justify-between gap-2 mt-4">
+        <Button variant="primary" isOutline={true}>
+          View Cart
+        </Button>
+        <Button size="normal">Checkout</Button>
+      </div>
+    </div>
+  );
+};
