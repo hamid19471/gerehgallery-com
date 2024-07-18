5242ce3e27ad8f7769f55e0cfb32499e18f7a0b1
Hamid Asadi <hamid.19471@gmail.com>
2024-07-18 14:06:00 +0330

    Update: add more sections to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,46 @@
-import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./_components/button";
+import { Divider } from "./_components/divider";
 import { Slider } from "./_components/slider";
+import { ShopSection, TopCategorySection } from "./_components/home-sections";
+import { OtherCategories } from "./_components/home-sections/other-categories/other-categories";
+import { ProductList } from "./product/_components";
 
 export default function Home() {
   return (
     <div>
       <Slider />
+      <Divider />
+      <div className="container flex items-center justify-between w-full mb-12">
+        <h1 className="font-bold text-4xl">Gereh Gallery Shop</h1>
+        <Link href={"/shop"}>
+          <Button size="large">View Shop</Button>
+        </Link>
+      </div>
+      <div className="mb-28">
+        <ShopSection />
+      </div>
+      <div className="container flex items-center justify-between w-full mb-12">
+        <h1 className="font-bold text-4xl">Top Categories</h1>
+        <Link href={"/categories"}>
+          <Button size="large">View All Categories</Button>
+        </Link>
+      </div>
+      <div>
+        <TopCategorySection />
+      </div>
+      <div className="container mt-28">
+        <OtherCategories />
+      </div>
+      <div className="container mt-28">
+        <div className="flex items-center w-full justify-between gap-3 mb-8">
+          <h3 className="font-bold text-4xl w-max">Top Products </h3>
+          <Link href={"/categories"}>
+            <Button size="large">View All</Button>
+          </Link>
+        </div>
+        <ProductList />
+      </div>
     </div>
   );
 }
